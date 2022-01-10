807d93f1022fbfc4ea3dfb091bcd95606af19823
Jay Hernandez <jeric614@yahoo.com>
2022-01-10 21:19:21 +0900

    image detail

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import ImageDetail from '../components/ImageDetail';
 
-const ImageScreen = ({ navigation }) => {
+const ImageScreen = () => {
   return (
     <View>
-      <Text>Image Screen</Text>
+      <ImageDetail />
+      <ImageDetail />
+      <ImageDetail />
     </View>
   );
 };
