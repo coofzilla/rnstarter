58968179b7fc5de7629f1721d0e8af7f92b23e8f
Jay Hernandez <jeric614@yahoo.com>
2022-01-12 12:07:45 +0900

    renamed vars to match convention

--- src/screens/RGBScreen.js
+++ src/screens/RGBScreen.js
@@ -9,25 +9,26 @@ const COLOR_INCREMENT = 10;
 const reducer = (state, action) => {
   //state === {red: number, green: number, blue: number };
   //action how object is changed
-  //action === {colorToChange: 'red' || 'green' || 'blue', amount}
-  //colorToChange is type
+  //action === {type: 'change_red' || 'change_green' || 'change_blue', payload}
+  //type is colorToChange
+  //payload is amount
 
-  switch (action.colorToChange) {
-    case 'red':
-      return state.red + action.amount > 255 || state.red + action.amount < 0
+  switch (action.type) {
+    case 'change_red':
+      return state.red + action.payload > 255 || state.red + action.payload < 0
         ? state
-        : { ...state, red: state.red + action.amount };
+        : { ...state, red: state.red + action.payload };
 
-    case 'green':
-      return state.green + action.amount > 255 ||
-        state.green + action.amount < 0
+    case 'change_green':
+      return state.green + action.payload > 255 ||
+        state.green + action.payload < 0
         ? state
-        : { ...state, green: state.green + action.amount };
-        
-    case 'blue':
-      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        : { ...state, green: state.green + action.payload };
+
+    case 'change_blue':
+      return state.blue + action.payload > 255 || state.blue + action.payload < 0
         ? state
-        : { ...state, blue: state.blue + action.amount };
+        : { ...state, blue: state.blue + action.payload };
     default:
       return state;
   }
@@ -43,28 +44,28 @@ const RGBScreen = () => {
       <ColorAdjuster
         color="Red"
         onIncrease={() =>
-          dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })
+          dispatch({ type: 'change_red', payload: COLOR_INCREMENT })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: 'red', amount: -COLOR_INCREMENT })
+          dispatch({ type: 'change_red', payload: -COLOR_INCREMENT })
         }
       />
       <ColorAdjuster
         color="Green"
         onIncrease={() =>
-          dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })
+          dispatch({ type: 'change_green', payload: COLOR_INCREMENT })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: 'green', amount: -COLOR_INCREMENT })
+          dispatch({ type: 'change_green', payload: -COLOR_INCREMENT })
         }
       />
       <ColorAdjuster
         color="Blue"
         onIncrease={() =>
-          dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })
+          dispatch({ type: 'change_blue', payload: COLOR_INCREMENT })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: 'blue', amount: -COLOR_INCREMENT })
+          dispatch({ type: 'change_blue', payload: -COLOR_INCREMENT })
         }
       />
       <View
