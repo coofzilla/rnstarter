c439abfe668ff2da64c3930965071de2c5cac3e9
Jay Hernandez <jeric614@yahoo.com>
2022-01-12 10:36:20 +0900

    tied state vals together

--- src/screens/RGBScreen.js
+++ src/screens/RGBScreen.js
@@ -7,28 +7,30 @@ const RGBScreen = () => {
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
+  const COLOR_INCREMENT = 10;
+
   return (
     <View>
       <ColorAdjuster
         color="Red"
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => setRed(red + COLOR_INCREMENT)}
+        onDecrease={() => setRed(red - COLOR_INCREMENT)}
       />
       <ColorAdjuster
         color="Green"
-        onIncrease={() => setGreen(green + 1)}
-        onDecrease={() => setRed(green - 1)}
+        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
+        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
       />
       <ColorAdjuster
         color="Blue"
-        onIncrease={() => setRed(blue + 1)}
-        onDecrease={() => setRed(blue - 1)}
+        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
+        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
       />
       <View
         style={{
           height: 100,
           width: 100,
-          backgroundColor: 'red',
+          backgroundColor: `rgb(${red},${green},${blue})`,
         }}
       />
     </View>
