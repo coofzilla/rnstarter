b8e4357d93bcdd79dda91119f775ad1402f38373
Jay Hernandez <jeric614@yahoo.com>
2022-01-13 22:28:21 +0900

    flex, justify

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,12 +3,26 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View>
-      <Text>BoxScreen</Text>
+    <View style={styles.view}>
+      <Text style={styles.text}>child 1</Text>
+      <Text style={styles.text}>child 2</Text>
+      <Text style={styles.text}>child 3</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  view: {
+    borderWidth: 3,
+    borderColor: 'black',
+    height: 200,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  text: {
+    borderWidth: 3,
+    borderColor: 'red',
+  },
+});
 
 export default BoxScreen;
